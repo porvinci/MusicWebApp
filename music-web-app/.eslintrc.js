83069387cc841f2b7933177002fa3ae8a79f37ac
jiang <porvinci@163.com>
2024-04-17 11:11:23 +0800

    图标字体&sass

--- music-web-app/.eslintrc.js
+++ music-web-app/.eslintrc.js
@@ -12,6 +12,27 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': 0,
+    'space-before-function-paren': 0,
+    'no-multiple-empty-lines': 'off',
+    'comma-dangle': ['error', {
+      // 默认的comma-dangle规则配置
+      arrays: 'always-multiline', // 或其他配置
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+    }],
+  },
+  overrides: [
+    {
+      // 匹配你要忽略规则的文件或目录
+      files: ['src/**/*.{vue,js}'],
+      rules: {
+        // 在这些文件中禁用comma-dangle规则
+        'comma-dangle': 'off',
+      },
+    },
+  ],
 }
