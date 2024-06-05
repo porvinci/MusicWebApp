38b5f7acd2bfc420fddaa0f50055b8ce20695dad
jiang <porvinci@163.com>
2024-06-05 15:41:57 +0800

    api/test.js 移位置

--- music-web-app/api/test.js
+++ music-web-app/api/test.js
@@ -10,7 +10,7 @@ module.exports = (req, res) => {
   // // 代理目标地址
   // // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   // if (req.url.startsWith('/backend')) {
-  //   target = 'http://backend-api.com'
+  //   target = 'https://musicpwa.vercel.app/'
   // }
 
   // // 创建代理对象并转发请求
